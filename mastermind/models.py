615303c9ac8ee2e0e6adb697df515086127f085a
Alex Maneu <mansoft@gmail.com>
2018-03-12 13:00:30 +0100

    Data model and migration

--- mastermind/models.py
+++ mastermind/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+GAME_STATUSES = (
+	('STARTED', 'Started'),
+	('FINISHED', 'Finished'),
+)
+
+GAME_WINNERS = (
+	('CODEMAKER', 'Codemaker'),
+	('CODEBREAKER', 'Codebreaker'),
+)
+
+class Game(models.Model):
+	started_at = models.DateTimeField(auto_now_add=True)
+	finished_at = models.DateTimeField(blank=True, null=True)
+	status = models.CharField(max_length=64, choices=GAME_STATUSES)
+	winner = models.CharField(max_length=64, choices=GAME_WINNERS, blank=True, null=True)
+	secret = models.TextField()
+	num_holes = models.IntegerField()
+	num_colors = models.IntegerField()
+	max_guesses = models.IntegerField()
+
+class Guess(models.Model):
+	game = models.ForeignKey(Game, on_delete=models.CASCADE)
+	created_at = models.DateTimeField(auto_now_add=True)
+	guess = models.TextField()
+	correct_position = models.IntegerField()
+	correct_color = models.IntegerField()
